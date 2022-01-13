78b780269f9fc794434143be361404b6f8b7a3a8
shubhangini <shubhanginijl@gmail.com>
2022-01-13 15:47:54 +0530

    this is my fetch todo app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FetchData from './Components/FetchData'
+import Home from './Components/Home';
+import "./index.css"
+
+const App = () => {
+    return (
+        <div>
+            {/* <FetchData/> */}
+            <Home/>
+        </div>
+    )
+}
+
+export default App
